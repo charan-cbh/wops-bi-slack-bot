8dca1135e19fa807d36652d5a37a46eb61ba1079
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-07 22:22:47 +0530

    Local files to GIT

--- scripts/clean_folder.sh
+++ scripts/clean_folder.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Clean up previous deployment files before CodeDeploy copies new ones
-rm -rf /home/ec2-user/wops-bi-slack-bot/*
+TARGET_DIR="/home/ec2-user/wops-bi-slack-bot"
+
+echo "[INFO] Cleaning up old app files from $TARGET_DIR"
+
+# Delete everything except the venv folder
+find "$TARGET_DIR" -mindepth 1 -not -path "$TARGET_DIR/venv*" -exec rm -rf {} + 2>> "$TARGET_DIR/clean_errors.log"
+
+echo "[INFO] Cleanup complete"
