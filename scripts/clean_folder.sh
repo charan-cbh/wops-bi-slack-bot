7b64bba468f718a2f77c85caa3ed0abaf8bee7a9
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-07 23:05:13 +0530

    Local files to GIT

--- scripts/clean_folder.sh
+++ scripts/clean_folder.sh
@@ -3,5 +3,12 @@
 TARGET="/home/ec2-user/wops-bi-slack-bot"
 echo "[INFO] Cleaning $TARGET"
 
-# Delete everything except virtual env and .git, with sudo to avoid permission issues
-sudo find "$TARGET" -mindepth 1 -not -name "venv" -not -name ".git" -exec rm -rf {} + 2>/dev/null || true
+# Delete everything inside TARGET except .env, venv, and .git
+sudo find "$TARGET" -mindepth 1 \
+  ! -name "venv" \
+  ! -name ".env" \
+  ! -name ".git" \
+  ! -path "$TARGET/venv/*" \
+  ! -path "$TARGET/.git/*" \
+  ! -name "." \
+  -exec rm -rf {} + 2>/dev/null || true
