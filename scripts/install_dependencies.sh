2f8d1905820760e644413414b54285ce98c0c3da
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-07 22:43:24 +0530

    Local files to GIT

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
 cd /home/ec2-user/wops-bi-slack-bot || exit 1
+
+# Create venv if not exists
+if [ ! -d "/home/ec2-user/venv" ]; then
+  python3 -m venv /home/ec2-user/venv
+fi
+
 source /home/ec2-user/venv/bin/activate || exit 1
 
-echo "[INFO] Installing Python dependencies..."
+# Upgrade pip & install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-
-echo "[INFO] Fixing permissions after install..."
-sudo chown -R ec2-user:ec2-user /home/ec2-user/wops-bi-slack-bot
-sudo chmod -R u+rw /home/ec2-user/wops-bi-slack-bot
