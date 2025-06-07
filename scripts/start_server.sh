58ddc952a9754c158d9e572904abce3e57d1afdc
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-07 16:59:23 +0530

    Local files to GIT

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/wops-bi-slack-bot
 source /home/ec2-user/venv/bin/activate
 
 # Kill previous uvicorn process if running
-pkill -f "uvicorn main:app" || true
+pkill -f "uvicorn app.main:app" || true
 
 # Run app in background
-nohup uvicorn main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
+nohup uvicorn app.main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
