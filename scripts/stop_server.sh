1f03d62137390e1b9a932eac7ccb1e893e3fd060
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-07 17:01:43 +0530

    Local files to GIT

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Stop any running instance of uvicorn
-pkill -f "uvicorn main:app" || true
 
-rm -rf /home/ec2-user/wops-bi-slack-bot/*
+# Activate the environment and stop the running uvicorn process
+cd /home/ec2-user/wops-bi-slack-bot
+source /home/ec2-user/venv/bin/activate
+
+# Kill uvicorn process safely
+pkill -f "uvicorn app.main:app" || true
