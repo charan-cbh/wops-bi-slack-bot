5e8cc6aaa18fd2f87f8296ff2e344e6949f799e8
Charan Mandali <charan.mandali@clipboardhealth.com>
2025-06-09 16:12:00 +0530

    V2 - Improve bot context with sample data

--- scripts/clean_folder.sh
+++ scripts/clean_folder.sh
@@ -3,11 +3,12 @@
 TARGET="/home/ec2-user/wops-bi-slack-bot"
 echo "[INFO] Cleaning $TARGET"
 
-# Delete everything inside TARGET except .env, venv, and .git
+# Delete everything inside TARGET except .env, venv, .git, and rsa_key.p8
 sudo find "$TARGET" -mindepth 1 \
   ! -name "venv" \
   ! -name ".env" \
   ! -name ".git" \
+  ! -name "rsa_key.p8" \
   ! -path "$TARGET/venv/*" \
   ! -path "$TARGET/.git/*" \
   ! -name "." \
